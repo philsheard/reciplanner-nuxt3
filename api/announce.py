209078f08e319ff131b92ab6d021aa871453e0e9
Phil Sheard <phil@philipsheard.co.uk>
2022-07-27 11:06:17 +0100

    Adding the python test function

---
+++ api/announce.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler
+from cowpy import cow
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        message = cow.Cowacter().milk('Nuxt and Python together at last')
+        self.wfile.write(message.encode())
+        return
